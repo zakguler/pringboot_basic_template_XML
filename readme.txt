3e67ceeb5a3b1968621beeb6de1f432160f60706
zguler <zguler@SEDOH-L01P-23U0.utah.utad.state.ut.us>
2018-04-03 14:50:31 -0600

    This is basic springboot template. use it to start a new spring boot project.

---
+++ readme.txt
@@ -0,0 +1,9 @@
+
+https://www.youtube.com/watch?v=gDHSLKmG8ZQ&index=12&list=PLmbC-xnvykcghOSOJ1ZF6ja3aOgZAgaMO
+
+-to run it: stop it first and then run CourseApiApp.java <======== run the application
+
+http://localhost:3000/topics
+
+
+
---
+++ src/main/java/com/zak/springboot/hello/HelloController.java
@@ -0,0 +1,17 @@
+package com.zak.springboot.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	//@RequestMapping("/hello")
+	@RequestMapping(value = "/hello", method = RequestMethod.GET,
+					produces = { "application/json", "application/xml" })
+	public String sayHi() {
+		return "Zak:3000...Hi from spring boot";
+	}
+	
+}
